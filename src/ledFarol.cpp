05a7f25c446d98efa3fd00337c434d33354bf36f
panama-021 <eduardo.l.santos18@aluno.senai.br>
2025-12-03 15:43:57 -0300

    Remove unused intensity setting and update logic

--- src/ledFarol.cpp
+++ src/ledFarol.cpp
@@ -8,49 +8,10 @@ void Led::begin()
         mcp.pinMode(i, OUTPUT);
 }
 
-void Led::setIntensidade(uint8_t intensidade)
-{
-    // 0–100%
-    intensidade = constrain(intensidade, 0, 100);
-
-    // Mapeia para 0–255
-    duty = map(intensidade, 0, 100, 0, 255);
-}
-
-
 void Led::update()
 {
- 
-    //  unsigned long now = micros();
-
-    // // Calcula tempos HIGH/LOW
-    // uint16_t tempoHigh = (periodo * duty) / 255;
-    // uint16_t tempoLow  = periodo - tempoHigh;
-
-    // // Estado atual HIGH → deve ir para LOW
-    // if (estado == true) {
-    //     if (now - lastMicros >= tempoHigh) {
-    //         estadoLed[2] = LOW;
-    //         estadoLed[1] = LOW;
-    //         estado = false;
-    //         lastMicros = now;
-    //     }
-    // }
-    // // Estado atual LOW → deve ir para HIGH
-    // else {
-    //     if (now - lastMicros >= tempoLow) {
-    //         estadoLed[2] = HIGH;
-    //         estadoLed[1] = HIGH;
-    //         estado = true;
-    //         lastMicros = now;
-    //     }
-    // }
- 
- 
     for (char i = 0; i < 6; i++)
         mcp.digitalWrite((i + 10), estadoLed[i]);
-
-
 }
 
 uint8_t Led::ligarFarol(uint8_t posicao)
@@ -115,17 +76,17 @@ uint8_t Led::desligarLed(uint8_t posicao)
 
     switch (posicao)
     {
-    case 1: // farol 
+    case 1: // farol
         estadoLed[2] = false;
         estadoLed[1] = false;
         break;
 
-    case 2: // seta 
+    case 2: // seta
         estadoLed[0] = false;
         estadoLed[3] = false;
         break;
 
-    case 3: // lanterna 
+    case 3: // lanterna
         estadoLed[4] = false;
         estadoLed[5] = false;
         break;
@@ -140,7 +101,7 @@ uint8_t Led::desligarLed(uint8_t posicao)
 uint8_t Led::piscarSeta(uint8_t posicao, unsigned long intervalo)
 {
     estadoSetaPisca = 1;
-    
+
     static unsigned long ultimoTempo = 0;
     static bool estadoPisca = false;
     unsigned long agora = millis();
@@ -149,25 +110,25 @@ uint8_t Led::piscarSeta(uint8_t posicao, unsigned long intervalo)
     {
         estadoPisca = !estadoPisca;
         ultimoTempo = agora;
-
-        switch (posicao)
-        {
-        case 1:
-            estadoLed[0] = estadoPisca;
-            estadoLed[3] = estadoPisca;
-            break;
-        case 2:
-            estadoLed[0] = estadoPisca;
-            // estadoLed[3] = false;
-            break;
-        case 3:
-            estadoLed[3] = estadoPisca;
-            // estadoLed[0] = false;
-            break;
-        }
-        
-        update();
+    }
+    
+    switch (posicao)
+    {
+    case 1:
+        estadoLed[0] = estadoPisca;
+        estadoLed[3] = estadoPisca;
+        break;
+    case 2:
+        estadoLed[0] = estadoPisca;
+        // estadoLed[3] = false;
+        break;
+    case 3:
+        estadoLed[3] = estadoPisca;
+        // estadoLed[0] = false;
+        break;
     }
 
+    update();
+    
     return estadoSetaPisca;
 }
